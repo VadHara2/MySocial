ddfbd52dc720de502dccc8acb07fe6463b3ed4c3
Vadym <VadHara2@gmail.com>
2021-09-01 11:08:13 +0300

    Dagger-Hilt Setup

---
+++ app/src/main/java/com/vadhara7/mysocialnetwork/SocialNetworkApplication.kt
@@ -0,0 +1,8 @@
+package com.vadhara7.mysocialnetwork
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SocialNetworkApplication:Application() {
+}
