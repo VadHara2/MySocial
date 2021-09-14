e9468e464819380a4b4c1d6fd17deac1a4decdec
Vadym <VadHara2@gmail.com>
2021-09-14 15:15:06 +0300

    setup home page

--- app/src/main/java/com/vadhara7/mysocialnetwork/ui/main/fragments/HomeFragment.kt
+++ app/src/main/java/com/vadhara7/mysocialnetwork/ui/main/fragments/HomeFragment.kt
@@ -1,7 +1,51 @@
 package com.vadhara7.mysocialnetwork.ui.main.fragments
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ProgressBar
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.vadhara7.mysocialnetwork.R
+import com.vadhara7.mysocialnetwork.databinding.FragmentHomeBinding
+import com.vadhara7.mysocialnetwork.ui.main.viewmodels.BasePostViewModel
+import com.vadhara7.mysocialnetwork.ui.main.viewmodels.HomeViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-class HomeFragment : Fragment(R.layout.fragment_home) {
+@AndroidEntryPoint
+class HomeFragment : BasePostFragment(R.layout.fragment_home) {
+    private lateinit var binding: FragmentHomeBinding
+
+    override val postProgressBar: ProgressBar
+        get() = binding.allPostsProgressBar
+
+    override val basePostViewModel: BasePostViewModel
+        get() {
+            val vm: HomeViewModel by viewModels()
+            return vm
+        }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() = binding.apply {
+        rvAllPosts.apply {
+            adapter = postAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+            itemAnimator = null
+        }
+    }
 }
